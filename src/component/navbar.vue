2dd40abe4ef774a04c4a78e7de160a1077d3e2a8
fortune-c <fortunate.adesinas@gmail.com>
2025-12-22 09:15:15 +0100

    navbar

---
+++ src/component/navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="sticky top-0 z-50 flex items-center justify-between p-4 text-white mx-10">
+    <a href="#">NullMax</a>
+    <ul class="flex flex-row space-x-4">
+      <li><a href="#home" class="hover:text-gray-700">Features</a></li>
+      <li><a href="#about" class="hover:text-gray-700">Color</a></li>
+      <li><a href="#contact" class="hover:text-gray-700">Pricing</a></li>
+      <li><a href="#contact" class="hover:text-gray-700">Contact</a></li>
+    </ul>
+    <a href="#" class="rounded-md bg-gray-600 px-2 text-xs w-20 h-5 text-center flex items-center justify-center hover:text-gray-400">Preorder</a>
+  </nav>
+</template>
